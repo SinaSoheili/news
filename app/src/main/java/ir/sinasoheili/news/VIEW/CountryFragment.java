656c0b3c98292b7b00a6c219d795c808ea3e367d
unknown <sinasoheili79@gmail.com>
2020-06-29 19:24:15 +0430

    create two fragment for to page in main activity

---
+++ app/src/main/java/ir/sinasoheili/news/VIEW/CountryFragment.java
@@ -0,0 +1,22 @@
+package ir.sinasoheili.news.VIEW;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import ir.sinasoheili.news.R;
+
+public class CountryFragment extends Fragment
+{
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.country_fragment_layout , container , false);
+    }
+}
